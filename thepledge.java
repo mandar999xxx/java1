8cb1f876a8945e340b3686f7667c73689681d6f9
Mandar shetty <mandarshetty999@gmail.com>
2026-02-20 20:39:14 +0530

    Update thepledge.java

--- thepledge.java
+++ thepledge.java
@@ -1,6 +1,6 @@
 public class thepledge {
     public static void main (String []args ){
-        System.out.println("My name is Mandar .");
+        System.out.println("My name is **** .");
         System.out.println(" I am commiting to the every code of the java every day ");
         System.out.println("9:30pm to 10:00pm is my time ");
         System.out.println("No mater whatever happens ! ");
@@ -16,3 +16,4 @@ public class thepledge {
         System.out.println("\n I WILL SUCCEED !: ");
     }
 }
+// this just for provoke my self not any other 
